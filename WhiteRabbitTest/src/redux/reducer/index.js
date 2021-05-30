8a96aed511b62a33e361d5c54fb8efe80ce9fe85
Anz Joseph <achembakessery@opentrends.net>
2021-05-30 13:58:33 +0530

    WIP: added persist in store

--- WhiteRabbitTest/src/redux/reducer/index.js
+++ WhiteRabbitTest/src/redux/reducer/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import dashboardReducer from "./dashboardReducer";
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['employeeArray']
+};
 const rootReducer = combineReducers({
-  dashboardReducer
+  dashboardReducer: persistReducer(persistConfig, dashboardReducer)
 });
 
 export default rootReducer;
